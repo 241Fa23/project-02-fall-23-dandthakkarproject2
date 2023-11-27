0bfcfb798becf680bc1d41f568f64d516ad9ae15
Jthakkar31 <145594730+Jthakkar31@users.noreply.github.com>
2023-11-27 17:20:39 -0600

    Created Player

---
+++ src/Player.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author S538302
+ */
+public class Player {
+    
+     private int health = 10;
+    private int score = 0;
+    
+    public Player(int health, int score){
+        this.health = health;
+        this.score = score;
+    }
+    
+    public abstract String getSpecialMove();
+    public abstract void useSpecialMove();
+    
+    
+    public void increaseScore(int amount) {
+        score += amount;
+    }
+
+    public void decreaseHealth() {
+        health--;
+    }
+
+    public boolean isAlive() {
+        return health > 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Health: " + health + "\nScore: " + score;
+    }
+
+    
+}
