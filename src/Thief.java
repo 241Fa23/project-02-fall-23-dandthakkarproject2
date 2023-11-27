2fe6eb005272dfe61a487db2dd1263a46c6f2a15
Azzuboy <126524783+Azzuboy@users.noreply.github.com>
2023-11-27 17:40:06 -0600

    Update Thief.java

--- src/Thief.java
+++ src/Thief.java
@@ -7,16 +7,25 @@
  *
  * @author S557474
  */
+<<<<<<< Updated upstream
 public class Thief extends Player {
     
     public Thief(int health, int score){
         super(health, score);
     }
     
+=======
+public class Thief extends Player{
+        public Thief(int health, int score){
+        super(health, score);
+    }
+   
+>>>>>>> Stashed changes
     public String getSpecialMove() {
         return "Sneak By";
     }
 
+<<<<<<< Updated upstream
     
     public void useSpecialMove() {
         System.out.println("The Thief sneaks by the foe, attempting to steal something valuable!");
@@ -26,4 +35,15 @@ public class Thief extends Player {
         return "\n" + "Thief Status: \n" + super.toString() + "\nThe Thief's special move is '" + getSpecialMove() + "'" + "\n";
     }
     
+=======
+   
+    public void useSpecialMove() {
+        System.out.println("The Thief sneaks by the foe, attempting to steal something valuable!");
+    }
+   
+    public String toString(){
+        return "\n" + "Thief Status: \n" + super.toString() + "\nThe Thief's special move is '" + getSpecialMove() + "'" + "\n";
+    }
+
+>>>>>>> Stashed changes
 }
