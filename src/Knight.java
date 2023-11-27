6666710c0c1f0f2ebc34971a3d768c0dbf58ea7f
Jthakkar31 <145594730+Jthakkar31@users.noreply.github.com>
2023-11-27 11:43:11 -0600

    Created Knight

---
+++ src/Knight.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author S538302
+ */
+public class Knight {
+    public Knight(int health, int score){
+        super(health, score);
+    }
+    
+    
+    public String getSpecialMove() {
+        return "Slash with Sword";
+    }
+
+    
+    public void useSpecialMove() {
+        System.out.println("The Knight slashes with his sword, striking the foe with a mighty blow!");
+    }
+    
+    public String toString(){
+        return "\n" + "Knight Status: \n" + super.toString() + "\nThe Knight's special move is '" + getSpecialMove() + "'" + "\n";
+    }
+    
+}
